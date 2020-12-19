1c6eaa32bb0f2c131c8179b91f708bcf83fb82f3
Petr Parolek <petr.parolek@gmail.com>
2020-12-19 13:10:00 +0100

    fist commit

---
+++ app/Presenters/HomepagePresenter.php
@@ -0,0 +1,67 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Presenters;
+
+use Nette\Application\UI\Form;
+use Nette\Application\UI\Presenter;
+
+final class HomepagePresenter extends Presenter
+{
+    public function beforeRender()
+    {
+        if ($this->isAjax() && (bool) $this->getParameter('isModal')) {
+            bdump('isAjax');
+            $this->payload->isModal = true;
+            $this->redrawControl("modal");
+        }
+    }
+
+    public function createComponentTestForm()
+    {
+        $form = new Form();
+
+        $form->addText('test', 'Test foo');
+
+        $form->onRender[] = [$this, 'makeBootstrap4'];
+
+        return $form;
+    }
+
+    public function makeBootstrap4(Form $form): void
+    {
+        $renderer = $form->getRenderer();
+        $renderer->wrappers['controls']['container'] = null;
+        $renderer->wrappers['pair']['container'] = 'div class="form-group row"';
+        $renderer->wrappers['pair']['.error'] = 'has-danger';
+        $renderer->wrappers['control']['container'] = 'div class=col-sm-9';
+        $renderer->wrappers['label']['container'] = 'div class="col-sm-3 col-form-label"';
+        $renderer->wrappers['control']['description'] = 'span class=form-text';
+        $renderer->wrappers['control']['errorcontainer'] = 'span class=form-control-feedback';
+        $renderer->wrappers['control']['.error'] = 'is-invalid';
+
+        foreach ($form->getControls() as $control) {
+            $type = $control->getOption('type');
+            if ($type === 'button') {
+                $control->getControlPrototype()->addClass(empty($usedPrimary) ? 'btn btn-primary' : 'btn btn-secondary');
+                $usedPrimary = true;
+
+            } elseif (in_array($type, ['text', 'textarea', 'select'], true)) {
+                $control->getControlPrototype()->addClass('form-control');
+
+            } elseif ($type === 'file') {
+                $control->getControlPrototype()->addClass('form-control-file');
+
+            } elseif (in_array($type, ['checkbox', 'radio'], true)) {
+                if ($control instanceof Nette\Forms\Controls\Checkbox) {
+                    $control->getLabelPrototype()->addClass('form-check-label');
+                } else {
+                    $control->getItemLabelPrototype()->addClass('form-check-label');
+                }
+                $control->getControlPrototype()->addClass('form-check-input');
+                $control->getSeparatorPrototype()->setName('div')->addClass('form-check');
+            }
+        }
+    }
+}
