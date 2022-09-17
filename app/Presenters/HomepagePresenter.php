d809f8eeffbb203b474c334744f341d7ef38adf5
Petr Parolek <petr.parolek@gmail.com>
2022-09-17 10:25:37 +0200

    added example form in modal with refresh form on page

--- app/Presenters/HomepagePresenter.php
+++ app/Presenters/HomepagePresenter.php
@@ -4,11 +4,19 @@ declare(strict_types=1);
 
 namespace App\Presenters;
 
+use Nette;
 use Nette\Application\UI\Form;
 use Nette\Application\UI\Presenter;
 
 final class HomepagePresenter extends Presenter
 {
+
+	/**
+	 * @var Nette\Database\Explorer
+	 * @inject
+	 */
+	public $database;
+
     public function beforeRender()
     {
         if ($this->isAjax() && (bool)$this->getParameter('isModal')) {
@@ -19,18 +27,66 @@ final class HomepagePresenter extends Presenter
         }
     }
 
-    public function createComponentTestForm()
+    public function createComponentTestForm(): Form
     {
         $form = new Form();
 
-        $form->addText('test', 'Test foo');
+        $form->addText('name', 'Název položky');
+
+		$form->addSubmit('ok','OK');
+
+		$form->getElementPrototype()->class('ajax');
+
+		$form->onSuccess[] = [$this, 'testFormSucceeded'];
 
         $form->onRender[] = [$this, 'makeBootstrap4'];
 
         return $form;
     }
 
-    public function makeBootstrap4(Form $form): void
+	public function testFormSucceeded(Form $form, Nette\Utils\ArrayHash $data): void
+	{
+		bdump($data);
+		$this->database->table('items')->insert($data);
+		$this->flashMessage('Položka byla úspěně uložena', 'success');
+
+		if ($this->isAjax()) {
+			$this->payload->showModal = false;
+			$this->redrawControl("flashes");
+		} else {
+			$this->redirect('Homepage:default');
+		}
+	}
+
+	public function handleRefresh(): void
+	{
+		$this->redrawControl('items');
+	}
+
+	public function createComponentItemsForm(): Form
+	{
+		$form = new Form();
+
+		$items = $this->database->table('items')->fetchPairs('id', 'name');
+		$form->addSelect('item', 'Položky', $items);
+
+		$form->addSubmit('ok','OK');
+
+		$form->onSuccess[] = [$this, 'itemsFormSucceeded'];
+
+		$form->onRender[] = [$this, 'makeBootstrap4'];
+
+		return $form;
+	}
+
+	public function itemsFormSucceeded(Form $form, Nette\Utils\ArrayHash $data): void
+	{
+		bdump($data);
+		$this->flashMessage('test abc', 'success');
+		$this->redirect('this');
+	}
+
+	public function makeBootstrap4(Form $form): void
     {
         $renderer = $form->getRenderer();
         $renderer->wrappers['controls']['container'] = null;
