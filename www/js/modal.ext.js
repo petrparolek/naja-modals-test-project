63c36e5b6799e26b41af21e67a7a6a392bb8f771
Pepa Linha <josef.linha@gmail.com>
2022-10-02 00:11:18 +0200

    Links with different urls for modals

--- www/js/modal.ext.js
+++ www/js/modal.ext.js
@@ -1,20 +1,34 @@
 /* global naja, $, intervalFunction */
 class ModalExtension {
 	initialize(naja) {
+		naja.uiHandler.addEventListener('interaction', this.handleInteraction.bind(this));
 		naja.addEventListener('complete', this.openModal.bind(this));
 	}
 
+	handleInteraction(event) {
+		const dataHref = event.detail.element.dataset.href;
+		if (dataHref) {
+			event.preventDefault();
+
+			// Create request by hand with data-href URL
+			naja.makeRequest('GET', dataHref);
+		}
+	}
+
 	openModal(event) {
-		let payload = event.detail.payload;
+		const payload = event.detail.payload || null;
 
 		if (payload === null || !payload.hasOwnProperty('modalId')) {
 			return;
 		}
-		let modalId = payload.modalId;
-		let showModal = payload.showModal;
-		if (showModal === undefined || showModal === false) {
+
+		const modalId = payload.modalId;
+		const showModal = payload.showModal || false;
+
+		if (!showModal) {
 			return;
 		}
+
 		$("#" + modalId).modal('show');
 	}
 }
